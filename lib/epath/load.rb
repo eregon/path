59e1887509faee3a14c72c4bf8548598cbb78ce7
Bernard Lambeau <blambeau@gmail.com>
2012-01-11 10:43:32 +0100

    Base implementation of Path#load

---
+++ lib/epath/load.rb
@@ -0,0 +1,17 @@
+class Path
+  #
+  # Path#load helps loading data from YAML, JSON and ruby files.
+  #
+  def load
+    case extname
+    when ".rb", ".ruby"
+      ::Kernel.eval(self.read, TOPLEVEL_BINDING, self.to_s)
+    when ".yml", ".yaml"
+      require 'yaml'
+      YAML.load(self.read)
+    when ".json"
+      require 'json'
+      JSON.load(self.read)
+    end
+  end
+end
