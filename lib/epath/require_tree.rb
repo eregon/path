4d2397c9a216e7565e3b5a3802cce2312e78c0e2
Benoit Daloze <eregontp@gmail.com>
2012-06-19 16:18:45 +0200

    improve Path.require_tree require order

--- lib/epath/require_tree.rb
+++ lib/epath/require_tree.rb
@@ -1,13 +1,33 @@
 class Path
   # @!group Requiring
 
-  # Requires all .rb files recursively (in alphabetic order)
-  # under +directory+ (or this file's directory if not given).
+  # Requires all .rb files recursively under +directory+
+  # (or this file's directory if not given).
+  #
+  # The order of requires is alphabetical,
+  # but files having the same basename as a directory
+  # are required before files in this directory.
+  #
+  #   # in bar.rb
+  #   Path.require_tree
+  #   # require in this order:
+  #   # foo.rb
+  #   # foo/ext.rb
+  #   # foo/sub.rb
+  #
   def self.require_tree(directory = nil)
     source = Path.file(caller)
     directory = Path.relative(directory || source.dir, caller)
 
-    directory.glob('**/*.rb').sort.each { |file|
+    directory.glob('**/*.rb').sort! { |a,b|
+      if b.inside?(a.rm_ext)
+        -1
+      elsif a.inside?(b.rm_ext)
+        +1
+      else
+        a <=> b
+      end
+    }.each { |file|
       require file.path unless source == file
     }
   end
--- spec/epath/require_tree_spec.rb
+++ spec/epath/require_tree_spec.rb
@@ -3,24 +3,38 @@ require 'spec_helper'
 describe 'Path#require_tree' do
   around(:each) do |example|
     Path.tmpdir('path-require-tree') do |dir|
-      %w[foo/foo1.rb
-         foo/foo2.rb
-         bar.rb
-         baz.rb
-        ].map { |path| (dir/path).touch! }
       @dir = dir
+      files.each(&:touch!)
       example.run
     end
   end
 
   let(:dir) { @dir }
+  let(:files) {
+    %w[bar.rb
+       baz.rb
+       foo.rb
+       foo/foo1.rb
+       foo/foo2.rb
+      ].map { |path| dir/path }
+  }
   let(:features) { $LOADED_FEATURES }
 
   it 'default directory' do
     expect {
       (dir/'bar.rb').write('Path.require_tree')
       require dir/:bar
-    }.to change { features.size }.by 4
+    }.to change { features.size }.by 5
+
+    features.last(5).map { |path|
+      Path(path) % dir
+    }.should == %w[
+      baz.rb
+      foo.rb
+      foo/foo1.rb
+      foo/foo2.rb
+      bar.rb
+    ].map(&Path)
   end
 
   it 'given directory' do
@@ -28,5 +42,9 @@ describe 'Path#require_tree' do
       (dir/'bar.rb').write('Path.require_tree "foo"')
       require dir/:bar
     }.to change { features.size }.by 3
+
+    features.last(3).map { |path|
+      Path(path) % dir
+    }.should == %w[foo/foo1.rb foo/foo2.rb bar.rb].map(&Path)
   end
 end
