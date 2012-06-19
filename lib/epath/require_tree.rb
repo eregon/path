0e4b81c5a8e721b5ffc1b78c7bf1e310f25090f1
Benoit Daloze <eregontp@gmail.com>
2012-06-19 15:46:32 +0200

    improve Path.require_tree spec and implementation
    * remove public method that should be private
    * expand once at the directory level
    * given directory should always be relative to current file dir

--- lib/epath/require_tree.rb
+++ lib/epath/require_tree.rb
@@ -4,19 +4,11 @@ class Path
   # Requires all .rb files recursively (in alphabetic order)
   # under +directory+ (or this file's directory if not given).
   def self.require_tree(directory = nil)
-    if directory
-      new(directory).require_tree
-    else
-      file = Path.file(caller)
-      file.dir.require_tree(file)
-    end
-  end
+    source = Path.file(caller)
+    directory = Path.relative(directory || source.dir, caller)
 
-  # @api private
-  # See {Path.require_tree}.
-  # It is not a real private method because {Path.require_tree}
-  # (so the {Path} class) needs to be able to call it.
-  def require_tree(source = nil)
-    glob('**/*.rb').sort.each { |file| require file.expand(dir).path unless file == source }
+    directory.glob('**/*.rb').sort.each { |file|
+      require file.path unless source == file
+    }
   end
 end
--- spec/epath/require_tree_spec.rb
+++ spec/epath/require_tree_spec.rb
@@ -2,26 +2,31 @@ require 'spec_helper'
 
 describe 'Path#require_tree' do
   around(:each) do |example|
-    Path.tmpchdir('path-test') do
-      %w[foo/foo1.rb foo/foo2.rb bar.rb].map(&Path).each(&:touch!)
+    Path.tmpdir('path-require-tree') do |dir|
+      %w[foo/foo1.rb
+         foo/foo2.rb
+         bar.rb
+         baz.rb
+        ].map { |path| (dir/path).touch! }
+      @dir = dir
       example.run
     end
   end
 
+  let(:dir) { @dir }
   let(:features) { $LOADED_FEATURES }
-
-  it 'given directory' do
-    expect { Path.require_tree 'foo' }.to change { features.size }.by 2
-  end
 
   it 'default directory' do
     expect {
-      Path('bar.rb').write('Path.require_tree')
-      require Path('bar.rb').expand
-    }.to change { features.size }.by 3
+      (dir/'bar.rb').write('Path.require_tree')
+      require dir/:bar
+    }.to change { features.size }.by 4
   end
 
-  it 'epath directory' do
-    expect { Path['foo'].require_tree }.to change { features.size }.by 2
+  it 'given directory' do
+    expect {
+      (dir/'bar.rb').write('Path.require_tree "foo"')
+      require dir/:bar
+    }.to change { features.size }.by 3
   end
 end
