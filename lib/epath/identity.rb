2e1072d068ded3326eead600e00410a3c97e69ca
Benoit Daloze <eregontp@gmail.com>
2012-06-24 19:20:19 +0200

    move #initialize and conversion methods to epath/identity.rb
    * corresponding specs are already in identity_spec.rb

--- lib/epath/identity.rb
+++ lib/epath/identity.rb
@@ -46,6 +46,37 @@ class Path
 
   # @!group Identity
 
+  # Creates a new Path.
+  # If multiple arguments are given, they are joined with File.join.
+  # The path will have File::ALT_SEPARATOR replaced with '/' and
+  # if it begins with a '~', it will be expanded (using File.expand_path).
+  # Accepts an Array of Strings, a Tempfile, anything that respond to #path,
+  # #to_path or #to_str with a String and defaults to calling #to_s.
+  #
+  # @param parts [Array<String>, Tempfile, #to_path, #path, #to_str, #to_s] the path-like object(s)
+  def initialize(*parts)
+    path = parts.size > 1 ? File.join(*parts) : parts.first
+    @path = case path
+    when Tempfile
+      @_tmpfile = path # We would not want it to be GC'd
+      path.path.dup
+    when String
+      path.dup
+    else
+      if path.respond_to? :to_path and String === path.to_path
+        path.to_path.dup
+      elsif path.respond_to? :path and String === path.path
+        path.path.dup
+      elsif path.respond_to? :to_str and String === path.to_str
+        path.to_str.dup
+      else
+        path.to_s.dup
+      end
+    end
+
+    init
+  end
+
   # Returns the +path+ as a String.
   # {#path} is implemented for better readability (+file.path+ instead of +file.to_s+) and as an accessor.
   # {#to_path} is implemented so Path objects are usable with +open+, etc.
@@ -82,6 +113,42 @@ class Path
   # A representation of the +path+.
   def inspect
     "#<Path #{@path}>"
+  end
+
+  # YAML loading.
+  def yaml_initialize(tag, ivars)
+    @path = ivars['path']
+    init
+  end
+
+  # Psych loading.
+  def init_with(coder)
+    @path = coder['path']
+    init
+  end
+
+  # JSON dumping.
+  def to_json(*args)
+    {
+      'json_class' => 'Path',
+      'data'       => @path
+    }.to_json(*args)
+  end
+
+  # JSON loading.
+  def self.json_create json
+    new json['data']
+  end
+
+  # Marshal dumping.
+  def marshal_dump
+    @path
+  end
+
+  # Marshal loading.
+  def marshal_load path
+    @path = path
+    init
   end
 end
 
