9ea5712c5c8fd7d8fe0b653f12c74a2f8da56c11
Benoit Daloze <eregontp@gmail.com>
2012-07-04 12:34:38 +0200

    start refactoring: move realpath internal implementation for old versions in compatibility.rb

--- lib/epath/implementation.rb
+++ lib/epath/implementation.rb
@@ -285,66 +285,6 @@ class Path
     end
   end
 
-  if File.respond_to?(:realpath) and File.respond_to?(:realdirpath)
-    def real_path_internal(strict = false, basedir = nil)
-      strict ? File.realpath(@path, basedir) : File.realdirpath(@path, basedir)
-    end
-  else
-    def realpath_rec(prefix, unresolved, h, strict, last = true)
-      resolved = []
-      until unresolved.empty?
-        n = unresolved.shift
-        if n == '.'
-          next
-        elsif n == '..'
-          resolved.pop
-        else
-          path = prepend_prefix(prefix, File.join(*(resolved + [n])))
-          if h.include? path
-            if h[path] == :resolving
-              raise Errno::ELOOP.new(path)
-            else
-              prefix, *resolved = h[path]
-            end
-          else
-            begin
-              s = File.lstat(path)
-            rescue Errno::ENOENT => e
-              raise e if strict || !last || !unresolved.empty?
-              resolved << n
-              break
-            end
-            if s.symlink?
-              h[path] = :resolving
-              link_prefix, link_names = split_names(File.readlink(path))
-              if link_prefix == ''
-                prefix, *resolved = h[path] = realpath_rec(prefix, resolved + link_names, h, strict, unresolved.empty?)
-              else
-                prefix, *resolved = h[path] = realpath_rec(link_prefix, link_names, h, strict, unresolved.empty?)
-              end
-            else
-              resolved << n
-              h[path] = [prefix, *resolved]
-            end
-          end
-        end
-      end
-      return prefix, *resolved
-    end
-
-    def real_path_internal(strict = false, basedir = nil)
-      path = @path
-      path = File.join(basedir, path) if basedir and relative?
-      prefix, names = split_names(path)
-      if prefix == ''
-        prefix, names2 = split_names(Dir.pwd)
-        names = names2 + names
-      end
-      prefix, *names = realpath_rec(prefix, names, {}, strict)
-      prepend_prefix(prefix, File.join(*names))
-    end
-  end
-
   def plus(path1, path2) # -> path
     prefix2 = path2
     index_list2 = []
